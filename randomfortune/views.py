98ce3083f33b88d14b3e7db482cd22a048f24cfc
unknown <GulAkberova>
2022-11-13 11:19:58 +0400

    first commit

---
+++ randomfortune/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import random
+
+fortunelist = [
+"Bugun super olacag!Inan ve tutdugun yoldan el cekme!",
+"Ola biler yorulmusan.Ancag dayanma.Ancag ireli",
+"Sen cox mesgulsan,ancag sen cox xowbextsen!",
+"Fikirlewme.Hereket et!",
+"Ola biler gunun pis kecir.Ancag bu pis heyatin var demek deyil!",
+"Her wey gozlediyinnen dahada yaxsi olacag!",
+"inan ve davam et",
+"Her wey yaxwi olacag!",
+"Her wey alt ust olacag!",
+"Allah belavi verecey.Gunah isleme!",
+'bol pul'
+]
+
+
+
+def fortune(request):
+    fortune=random.choice(fortunelist)
+    context={
+        "fortune":fortune
+    }
+    return render(request,"randomfortune/fortune.html",context)
+
+
